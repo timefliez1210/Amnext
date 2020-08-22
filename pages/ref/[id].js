2bcdf226a0f66f5df5f11bf05e2b3198c3cc49a5
timefliez1210 <crfabig@gmail.com>
2020-08-22 12:10:47 +0800

    maybe dynamic fix

--- pages/ref/[id].js
+++ pages/ref/[id].js
@@ -5,7 +5,7 @@ import { ABI, ADDRESS, OWNER } from "../../utils/globals";
 import { loadWeb3 } from "../../utils/utility";
 
 class RefLink extends Component {
-  static async getInitialProps(ctx) {
+  static async getServerSideProps(ctx) {
     const res = await fetch("https://api.github.com/repos/vercel/next.js");
     const json = await res.json();
     return { stars: json.stargazers_count };
